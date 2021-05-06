68a64d7494f5d42ce3ccde0fcc98d307fa932f13
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-05-06 21:54:44 +0200

    Added models

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cwiczenia4_zen_s19743.Models
+{
+    public class Order
+    {
+        public int IdOrder { get; set; }  
+        public int IdProduct { get; set; }
+        public int Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? FulfilledAt { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace cwiczenia4_zen_s19743.Models
+{
+    public class Product
+    {
+        public int IdProduct { get; set; }  
+        public string Name { get; set; }  
+        public string Description { get; set; }  
+        public string Price { get; set; }  
+    }
+}
