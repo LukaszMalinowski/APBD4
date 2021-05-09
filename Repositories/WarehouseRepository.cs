5d1632e813f977744846fd89f864c6fe06d2afc3
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-05-09 20:57:01 +0200

    Added checking if warehouse and product exists

--- Repositories/WarehouseRepository.cs
+++ Repositories/WarehouseRepository.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using cwiczenia4_zen_s19743.Models.DTOs;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace cwiczenia4_zen_s19743.Repositories
 {
     public class WarehouseRepository : IWarehouseRepository
     {
+        private readonly IConfiguration _configuration;
+
+        public WarehouseRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
         public int RegisterProductAtWarehouse(WarehouseDTO warehouseDto)
         {
             throw new System.NotImplementedException();
@@ -12,12 +21,38 @@ namespace cwiczenia4_zen_s19743.Repositories
 
         public bool ProductExists(int? idProduct)
         {
-            throw new System.NotImplementedException();
+            using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("PjatkDb"));
+            SqlCommand command = new SqlCommand
+            {
+                Connection = connection,
+                CommandText = "SELECT COUNT(*) FROM Product WHERE IdProduct = @IdProduct"
+            };
+
+            command.Parameters.AddWithValue("@IdProduct", idProduct);
+            
+            connection.Open();
+
+            int productCount = (int) command.ExecuteScalar();
+
+            return productCount > 0;
         }
 
         public bool WarehouseExists(int? idWarehouse)
         {
-            throw new System.NotImplementedException();
+            using SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("PjatkDb"));
+            SqlCommand command = new SqlCommand
+            {
+                Connection = connection,
+                CommandText = "SELECT COUNT(*) FROM Warehouse WHERE IdWarehouse = @IdWarehouse"
+            };
+
+            command.Parameters.AddWithValue("@IdWarehouse", idWarehouse);
+            
+            connection.Open();
+
+            int warehouseCount = (int) command.ExecuteScalar();
+
+            return warehouseCount > 0;
         }
 
         public int GetOrderId(int? idProduct, int? amount, DateTime? createdAt)
